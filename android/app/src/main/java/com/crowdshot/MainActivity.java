24fc895205240dd57c77593478378b6a174771d9
Kenneth Ma <ken@downforce.ca>
2016-11-01 20:41:55 -0400

    Added: Vector Icons

--- android/app/src/main/java/com/crowdshot/MainActivity.java
+++ android/app/src/main/java/com/crowdshot/MainActivity.java
@@ -1,6 +1,7 @@
 package com.crowdshot;
 
 import com.facebook.react.ReactActivity;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 
 public class MainActivity extends ReactActivity {
