f644a52e9b54387fdf875fdd02434a96f1ff5f16
Kenneth Ma <ken@downforce.ca>
2016-11-02 20:16:21 -0400

    Added: Template JS for all Components

---
+++ src/components/Template.js
@@ -0,0 +1,23 @@
+import React, {
+  Component
+} from 'react';
+import {
+  View, StyleSheet
+} from 'react-native';
+import {
+  Colors, Sizes
+} from '../../Const';
+
+export default class ContestCard extends Component {
+  render() {
+    return (
+      <View />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  }
+});
