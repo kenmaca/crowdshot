45ee5ef1a9355f6762099d08de1f9f218ff88bc5
Kenneth Ma <ken@downforce.ca>
2016-11-07 13:48:33 -0500

    Added: WIP Card Direct Switcher with animation

--- src/views/contests/ContestPhotos.js
+++ src/views/contests/ContestPhotos.js
@@ -2,7 +2,8 @@ import React, {
   Component
 } from 'react';
 import {
-  View, StyleSheet, Alert, Text
+  View, StyleSheet, Alert, Text, ListView, TouchableOpacity,
+  Animated
 } from 'react-native';
 import {
   Colors, Sizes
@@ -12,45 +13,116 @@ import {
 import SwipeCards from 'react-native-swipe-cards';
 import ContestPhotoCard from '../../components/lists/ContestPhotoCard';
 import CloseFullscreenButton from '../../components/common/CloseFullscreenButton';
+import Button from '../../components/common/Button';
 
 export default class ContestPhotos extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cards: [
-        {selected: false}, {selected: true}, {}, {}
-      ]
+      endDate: 1234,
+      prizes: {
+        '1234': true,
+        '1235': true
+      },
+      referencePhotoId: 'appLoginBackground',
+      instructions: 'stuff',
+      locationId: 'location',
+      entries: {
+        abc1: true,
+        abc2: true,
+        abc3: true
+      },
+      thumbnails: new ListView.DataSource({
+        rowHasChanged: (r1, r2) => r1 !== r2
+      })
     };
+  }
+
+  componentWillMount() {
+    this.setState({
+      thumbnails: this.state.thumbnails.cloneWithRows(
+        Object.keys(this.state.entries)
+      )
+    });
   }
 
   render() {
     return (
       <View style={styles.container}>
         <SwipeCards
+          ref='swiper'
           containerStyle={styles.cards}
-          cards={this.state.cards}
+          cards={Object.keys(this.state.entries)}
           renderCard={entry => (
             <ContestPhotoCard
-              {...entry}
-              i={this.state.cards.indexOf(entry) + 1}
-              n={this.state.cards.length} />
+              i={
+                Object.keys(
+                  this.state.entries
+                ).indexOf(entry) + 1
+              }
+              n={
+                Object.keys(this.state.entries).length
+              } />
           )}
           yupText='Shortlist!'
           noText='Nope!'
+          loop
           handleYup={entry => {
-            entry.selected = true;
+            entry
           }}
           handleNope={entry => {
-            entry.selected = false;
+            entry
           }}
           cardRemoved={i => {
 
             // handle end of selection
-            if (i >= this.state.cards.length - 1) {
-              Alert.alert('end');
+            if (i >= Object.keys(this.state.entries).length - 1) {
             }
           }} />
         <CloseFullscreenButton />
+        <ListView
+          horizontal
+          pagingEnabled
+          dataSource={this.state.thumbnails}
+          style={styles.thumbnailContainer}
+          contentContainerStyle={styles.thumbnails}
+          renderRow={data => {
+            return (
+              <TouchableOpacity
+                onPress={() => {
+
+                  // shortcut to card directly
+                  Animated.decay(
+                    this.refs.swiper.state.pan,
+                    {
+                      velocity: {x: 0, y: 12},
+                      deceleration: 0.98
+                    }
+                  ).start(
+                    () => {
+                      this.refs.swiper._resetState.bind(
+                        this.refs.swiper
+                      )();
+                      this.refs.swiper.setState({
+                        card: data
+                      });
+                    }
+                  );
+                }}>
+                <View style={{
+                  backgroundColor: Colors.Primary,
+                  width: 50, height: 50, margin: 1
+                }} />
+              </TouchableOpacity>
+            );
+          }} />
+        <Button
+          squareBorders
+          fontAwesome
+          icon='trophy'
+          style={styles.endButton}
+          color={Colors.Primary}
+          label='End Contest & Announce Winners' />
       </View>
     );
   }
@@ -65,19 +137,24 @@ const styles = StyleSheet.create({
   },
 
   cards: {
-    flex: 1,
     marginTop: Sizes.Height / 7,
+    marginBottom: Sizes.OuterFrame * 1.25,
     backgroundColor: Colors.Transparent
   },
 
-  noMoreContainer: {
-    flex: 1,
-    marginTop: -Sizes.Height / 7,
-    alignItems: 'center',
-    justifyContent: 'center'
+  thumbnailContainer: {
+    padding: Sizes.InnerFrame,
+    alignSelf: 'stretch',
+    backgroundColor: Colors.Foreground
   },
 
-  noMore: {
-    fontSize: Sizes.H1 * 2
+  thumbnails: {
+    alignItems: 'flex-end'
+  },
+
+  endButton: {
+    alignSelf: 'stretch',
+    paddingTop: Sizes.InnerFrame,
+    paddingBottom: Sizes.InnerFrame
   }
 });
