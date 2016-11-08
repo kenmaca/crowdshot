7eca2aad02ad7fcff963c0fd8d09e4bca1e0856b
terencelee <kisceneret@gmail.com>
2016-11-07 20:42:18 -0500

    Added: mapview

---
+++ src/components/contestant/ContestMapView.js
@@ -0,0 +1,76 @@
+import React, {
+  Component
+} from 'react';
+import {
+  StyleSheet, View, Image, Alert, TouchableOpacity, Modal
+} from 'react-native';
+import {
+  Sizes, Colors
+} from '../../Const';
+
+// components
+import MapView from 'react-native-maps';
+
+
+export default class ContestMapView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: {
+        coords: {
+          latitude: 0,
+          longitude: 0,
+        }
+      }
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({location: position});
+      },
+      (error) => alert(JSON.stringify(error)),
+        {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          scrollEnabled={true}
+          region={{
+            latitude: this.state.location.coords.latitude,
+            longitude: this.state.location.coords.longitude,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01
+          }}>
+          <MapView.Marker
+            coordinate={{
+              latitude: this.state.location.coords.latitude,
+              longitude: this.state.location.coords.longitude,
+            }}
+            pinColor={Colors.Primary}
+          />
+        </MapView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignSelf: 'stretch',
+    marginBottom: 50,
+  },
+
+  map: {
+    alignSelf: 'stretch',
+    height: Sizes.Height - 50
+  },
+
+
+});
