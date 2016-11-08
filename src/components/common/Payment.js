56f65b0fc73c32ba39b3feee455a5eced056ae3b
Roy <Roylaw.w@gmail.com>
2016-11-08 01:41:03 -0500

    Changed: added swiper to enter informations

--- src/components/common/Payment.js
+++ src/components/common/Payment.js
@@ -2,76 +2,212 @@ import React, {
   Component
 } from 'react';
 import {
-  StyleSheet, View, Text
+  View,
+Text,
+TouchableOpacity,
+StyleSheet,
+ScrollView,
+Dimensions,
+TextInput,
+Image
 } from 'react-native';
 import {
   Sizes, Colors
 } from '../../Const';
-
-import CreditCard from 'react-native-credit-card';
 import {
-  CreditCardInput, LiteCreditCardInput
-} from 'react-native-credit-card-input';
+  Actions
+} from 'react-native-router-flux';
+
+// components
+import CreditCard, {CardImages} from 'react-native-credit-card';
 import Button from '../../components/common/Button';
 import Field from '../../components/common/Field';
+import NewContest from '../../views/forms/NewContest';
+import PaymentField from './PaymentField';
+import Swiper from 'react-native-swiper';
+
+
+const SWIPER_HEIGHT = 180;
+const {height, width} = Dimensions.get('window');
 
 export default class Payment extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      type: props.type,
-      focused: props.focused,
-      number: props.number,
-      name: props.name,
-      expiry: props.expiry,
-      cvc: props.cvc
+      focused: 'name',
+      number: '',
+      cvc: '',
+      expiry:'',
+      index: 0,
+      type: 'visa'
     }
   }
 
+  onNext() {
+    this.swiper.scrollby(1);
+  }
+
+  componentDidMount() {
+    this.refs['number'].focus();
+  }
+
+
+  onMomentumScrollEnd(e, state, context) {
+    var indexMap = [
+        'number',
+        'name',
+        'expiry',
+        'cvc',
+        'type',
+    ];
+    this.setState({
+        index: state.index,
+        focused: indexMap[state.index]
+    }, () => {
+        try {
+            this.refs[indexMap[state.index]].focus();
+        } catch(e) {
+
+        }
+    });
+  }
 
   render() {
+    var cardTypes = [];
+    for (var key in CardImages) {
+        cardTypes.push({type: key, image: CardImages[key]});
+    }
+
+    if (this.state.restoring) {
+        return null;
+    }
+
     return (
-      <View style={styles.container}>
-        <View style={styles.creditContainer}>
+    <View style={styles.container}>
+
           <CreditCard
-              type={this.state.type}
-              imageFront={require('../../../res/img/card-front.png')}
-              imageBack={require('../../../res/img/card-back.png')}
-              shiny={true}
-              bar={true}
-              focused={this.state.focused}
-              number={this.state.number}
-              name={this.state.name}
-              expiry={this.state.expiry}
-              cvc={this.state.cvc}/>
-        </View>
-        <View style={styles.creditContainer}>
-          <CreditCardInput onChange={this._onChange}/>
-        </View>
+            style={{marginVertical: 10, marginHorizontal: 10, marginBottom: 0, elevation: 3, alignSelf: 'center'}}
+            imageFront={require('../../../res/img/card-front.png')}
+            imageBack={require('../../../res/img/card-back.png')}
+            shiny={false}
+            bar={false}
+            focused={this.state.focused}
+            number={this.state.number}
+            name={this.state.name}
+            expiry={this.state.expiry}
+            cvc={this.state.cvc}/>
+          <Swiper
+            style={styles.wrapper}
+            height={SWIPER_HEIGHT}
+            showsButtons={false}
+            onMomentumScrollEnd = {this.onMomentumScrollEnd.bind(this)}
+            ref={(swiper) => {this.swiper = swiper}}
+            index={this.state.index}>
+            <View style={styles.slide}>
+                <View style={styles.card}>
+                    <Text style={styles.textNumber}>CARD NUMBER</Text>
+                    <TextInput
+                      ref="number"
+                      autoFocus={true}
+                      value={this.state.number}
+                      onChangeText={(number) => this.setState({
+                          number
+                        })}/>
+                </View>
+            </View>
+            <View style={styles.slide}>
+                <View style={styles.card}>
+                    <Text style={styles.textName}>CARD HOLDER'S NAME</Text>
+                    <TextInput
+                      ref="name"
+                      value={this.state.name}
+                      onChangeText={(name) => this.setState({
+                        name
+                      })}/>
+                </View>
+            </View>
+            <View style={styles.slide}>
+                <View style={styles.card}>
+                    <Text style={styles.textName}>EXPIRY</Text>
+                    <TextInput
+                      ref="expiry"
+                      value={this.state.expiry}
+                      onChangeText={(expiry) => this.setState({
+                        expiry
+                      })}/>
+                </View>
+            </View>
+            <View style={styles.slide}>
+                <View style={styles.card}>
+                    <Text style={styles.textCvc}>CVV/CVC NUMBER</Text>
+                    <TextInput
+                      ref="cvc"
+                      value={this.state.cvc}
+                      onChangeText={(cvc) => this.setState({
+                        cvc
+                      })}/>
+                </View>
+            </View>
+            <View style={styles.slide}>
+                <View style={styles.card}>
+                    <Text style={styles.textNumber}>CARD TYPE</Text>
+                    <View style={{flexDirection: 'row'}}>
+                        {cardTypes.map((cardType) => {
+                            return (
+                                <TouchableOpacity
+                                  key={cardType.type}
+                                  onPress={() => this.setState({
+                                      type: cardType.type
+                                  })}>
+                                    <View>
+                                        <Image
+                                          source={{uri: cardType.image}}                                            style={styles.img} />
+                                    </View>
+                                </TouchableOpacity>
+                            );
+                        })}
+                    </View>
+                </View>
+            </View>
+        </Swiper>
         <Button
           color={Colors.Primary}
-          label='Pay'/>
-      </View>
+          label='Pay'
+          squareBorders={9}
+          style={styles.buttonStyle}/>
+    </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    backgroundColor: Colors.Background
+      backgroundColor: Colors.Background,
+      flex: 1,
+      paddingTop: 30
   },
-  creditContainer: {
-    height: 40,
-    width: Sizes.width - 10,
-    backgroundColor: Colors.Background,
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignSelf: 'center'
+  wrapper: {
+      height: SWIPER_HEIGHT,
+      backgroundColor: Colors.Background
   },
-  button: {
-
+  slide: {
+      height: SWIPER_HEIGHT,
+      justifyContent: 'center',
+      alignItems: 'center'
+  },
+  text: {
+    color: Colors.Text,
+    fontSize: Sizes.Text
+  },
+  card: {
+      marginHorizontal: 10,
+      marginBottom: 30,
+      backgroundColor: Colors.ModalBackground,
+      borderRadius: 3,
+      elevation: 3,
+      borderBottomWidth: 1,
+      borderRightWidth: 1,
+      borderColor: '#ddd',
+      padding: 10,
   }
 })
