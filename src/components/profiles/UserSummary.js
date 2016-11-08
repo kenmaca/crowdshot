1f21b012e5335ac682692ea230fecf5e37abef0c
Kenneth Ma <ken@downforce.ca>
2016-11-07 19:27:48 -0500

    Added: Missing files from last commits

---
+++ src/components/profiles/UserSummary.js
@@ -0,0 +1,125 @@
+import React, {
+  Component
+} from 'react';
+import {
+  View, StyleSheet, Text
+} from 'react-native';
+import {
+  Colors, Sizes
+} from '../../Const';
+import Database from '../../utils/Database';
+
+// components
+import Avatar from './Avatar';
+
+export default class UserSummary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayName: 'Unknown'
+    };
+  }
+
+  componentDidMount() {
+    Database.ref(`profiles/${this.props.uid}`).once(
+      'value',
+      data => {
+        if (data.exists()) {
+          this.setState({
+            ...data.val()
+          });
+        }
+      }
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.avatar}>
+          <Avatar
+            outline
+            outlineColor={Colors.ModalBackground}
+            size={48}
+            uid={this.props.uid} />
+        </View>
+        <Text style={styles.name}>
+          {this.state.displayName}
+        </Text>
+        <View style={styles.userContainer}>
+          <View style={styles.statContainer}>
+            <Text style={styles.statTitle}>
+              24
+            </Text>
+            <Text style={styles.statDescription}>
+              CONTESTS WON
+            </Text>
+          </View>
+          <View style={styles.statContainer}>
+            <Text style={styles.statTitle}>
+              66%
+            </Text>
+            <Text style={styles.statDescription}>
+              SUCCESS RATE
+            </Text>
+          </View>
+          <View style={styles.statContainer}>
+            <Text style={styles.statTitle}>
+              382
+            </Text>
+            <Text style={styles.statDescription}>
+              PHOTOS
+            </Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    minHeight: Sizes.InnerFrame * 5,
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    backgroundColor: Colors.ModalBackground,
+    borderRadius: 5
+  },
+
+  avatar: {
+    marginTop: -Sizes.InnerFrame * 1.5
+  },
+
+  userContainer: {
+    flexDirection: 'row',
+    padding: Sizes.InnerFrame,
+    alignSelf: 'stretch',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
+    backgroundColor: Colors.Foreground,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5
+  },
+
+  name: {
+    padding: Sizes.InnerFrame / 2,
+    paddingBottom: Sizes.InnerFrame / 1.5,
+    fontSize: Sizes.H3,
+    fontWeight: '500'
+  },
+
+  statContainer: {
+  },
+
+  statTitle: {
+    fontSize: Sizes.H4,
+    fontWeight: '500',
+    color: Colors.SubduedText
+  },
+
+  statDescription: {
+    fontSize: Sizes.SmallText,
+    fontWeight: '100',
+    color: Colors.SubduedText
+  }
+});
