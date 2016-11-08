c757bbda1bc453f220b94cb4cae60dda4fd13f81
terencelee <kisceneret@gmail.com>
2016-11-08 02:13:04 -0500

    added: dummy photo locations

--- src/components/contestant/ContestMapView.js
+++ src/components/contestant/ContestMapView.js
@@ -21,7 +21,7 @@ export default class ContestMapView extends Component {
           latitude: 0,
           longitude: 0,
         }
-      }
+      },
     };
   }
 
@@ -37,7 +37,10 @@ export default class ContestMapView extends Component {
         {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
     );
     navigator.geolocation.watchPosition((position) => {
-      this.setState({location: position});
+      this.setState({
+        location: position,
+        init: true
+      });
     });
   }
 
@@ -62,6 +65,27 @@ export default class ContestMapView extends Component {
             pinColor={Colors.Primary}
           />
           }
+          <MapView.Marker
+            coordinate={{
+              latitude: this.state.location.coords.latitude + 0.002,
+              longitude: this.state.location.coords.longitude + 0.004,
+            }}
+            image={require('../../../res/img/camera.png')}
+          />
+          <MapView.Marker
+            coordinate={{
+              latitude: this.state.location.coords.latitude - 0.002,
+              longitude: this.state.location.coords.longitude + 0.002,
+            }}
+            image={require('../../../res/img/camera.png')}
+          />
+          <MapView.Marker
+            coordinate={{
+              latitude: this.state.location.coords.latitude + 0.003,
+              longitude: this.state.location.coords.longitude + 0.001,
+            }}
+            image={require('../../../res/img/camera.png')}
+          />
         </MapView>
         {this.state.init ||
         <View style={styles.buttonContainer}>
