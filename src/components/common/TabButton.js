823d99a8b0a17ea70c593a48959c0580a424ac95
Kenneth Ma <ken@downforce.ca>
2016-11-21 00:22:24 -0500

    Added: Unread Counters to TabButtons, but no connection to live data due to undecided way of handling counters, WIP

--- src/components/common/TabButton.js
+++ src/components/common/TabButton.js
@@ -1,5 +1,5 @@
 import React, {
-  PropTypes,
+  Component,
 } from 'react';
 import {
   View, Text, StyleSheet
@@ -11,33 +11,59 @@ import {
 // components
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-const propTypes = {
-  selected: PropTypes.bool,
-  title: PropTypes.string,
-  iconName: PropTypes.string
-};
+export default class TabButton extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}>
+        <View>
+          <Icon
+            name={this.props.iconName}
+            size={Sizes.H3}
+            color={
+              this.props.selected ? Colors.Primary: Colors.Text
+            } />
+          {
+            this.props.unread > 0 && (
+              <View style={styles.unreadContainer}>
+                <Text style={styles.unread}>
+                  {this.props.unread}
+                </Text>
+              </View>
+            )
+          }
+        </View>
+        <Text
+          style={{
+            fontSize: Sizes.SmallText,
+            color: this.props.selected ? Colors.Primary: Colors.Text
+          }}>
+          {this.props.title}
+        </Text>
+      </View>
+    );
+  }
+}
 
-const TabButton = props => (
-  <View style={{
+const styles = StyleSheet.create({
+  unreadContainer: {
+    position: 'absolute',
+    top: -5,
+    right: -10,
+    alignSelf: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center'
-  }}>
-    <Icon
-      name={props.iconName}
-      size={Sizes.H3}
-      color={
-        props.selected ? Colors.Primary: Colors.Text
-      } />
-    <Text
-      style={{
-        fontSize: Sizes.SmallText,
-        color: props.selected ? Colors.Primary: Colors.Text
-      }}>
-      {props.title}
-    </Text>
-  </View>
-);
+    padding: Sizes.InnerFrame / 4,
+    paddingTop: Sizes.InnerFrame / 6,
+    paddingBottom: Sizes.InnerFrame / 6,
+    borderRadius: 20,
+    backgroundColor: Colors.Cancel
+  },
 
-TabButton.propTypes = propTypes;
-
-export default TabButton;
+  unread: {
+    fontSize: Sizes.SmallText,
+    color: Colors.Text
+  }
+});
