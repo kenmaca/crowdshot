867d49a4e763d7c2e45a059e3c55622c72566a61
terencelee <kisceneret@gmail.com>
2016-11-25 23:10:10 -0500

    missed a line

--- src/components/contestant/ContestMapView.js
+++ src/components/contestant/ContestMapView.js
@@ -30,7 +30,7 @@ export default class ContestMapView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loaded: false,
+      updated: false,
       profile: {},
       region: {
 
@@ -73,7 +73,7 @@ export default class ContestMapView extends Component {
   }
 
   onRegionChange(region, initial) {
-    if (!initial || !this.state.loaded) {
+    if (!initial || !this.state.updated) {
       this.ref.updateCriteria({
         center: [
           region.latitude,
@@ -217,7 +217,7 @@ export default class ContestMapView extends Component {
                 <View style={styles.shadow}>
                   <View style={styles.textContainer}>
                     <Text style={styles.text}>
-                      {this.state.loaded
+                      {this.state.updated
                         ? "No active contests found — try moving the map around"
                         : "Looking for contests"}
                     </Text>
