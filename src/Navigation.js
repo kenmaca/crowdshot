b317c9213fe49173e6a6a02f849786447143a600
Roy <Roylaw.w@gmail.com>
2016-11-27 01:49:08 -0500

    Added: Crowd Chat Tabbutton added for

--- src/Navigation.js
+++ src/Navigation.js
@@ -160,6 +160,13 @@ export default class Navigation extends Component {
                 title='Join a Contest'
                 iconName='camera'
                 icon={TabButton} />
+              <Scene
+                hideNavBar
+                key='mainChat'
+                component={ChatRoom}
+                title='Crowd Chat'
+                iconName='chat-bubble'
+                icon={TabButton} />
             </Scene>
           </Scene>
         </Router>
