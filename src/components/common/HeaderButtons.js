68b0357aa0c7a754076a112f3d6d4a8e23c245b5
Kenneth Ma <ken@kenma.ca>
2016-12-03 01:14:00 -0500

    Fixed: HeaderButtons blocking off too much of screen

--- src/components/common/HeaderButtons.js
+++ src/components/common/HeaderButtons.js
@@ -8,27 +8,12 @@ import {
   Colors, Sizes
 } from '../../Const';
 
-// components
-import LinearGradient from 'react-native-linear-gradient';
-
 export default class HeaderButtons extends Component {
   render() {
     return (
-      <LinearGradient
-        {...this.props}
-        colors={[
-          Colors.MediumDarkOverlay,
-          Colors.Transparent,
-          Colors.Transparent
-        ]}
-        style={[
-          styles.container,
-          this.props.style
-        ]}>
-        <View
-          children={this.props.children}
-          style={styles.buttonContainer} />
-      </LinearGradient>
+      <View
+        children={this.props.children}
+        style={styles.container} />
     );
   }
 }
@@ -38,11 +23,10 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 0,
     width: Sizes.Width,
-    minHeight: Sizes.Height * 0.2,
     padding: Sizes.InnerFrame,
     paddingTop: Sizes.OuterFrame,
     flexDirection: 'row',
-    alignItems: 'flex-start',
+    alignItems: 'flex-end',
     justifyContent: 'flex-end'
   },
 
