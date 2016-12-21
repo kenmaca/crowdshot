328885494cb32259e1f69c822f410904443c6c70
Kenneth Ma <ken@kenma.ca>
2016-12-21 18:16:56 -0500

    Fixed: More renaming

--- ios/Pods/Target Support Files/Pods-Crowdshot/Pods-crowdshot-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Crowdshot : NSObject
-@end
-@implementation PodsDummy_Pods_Crowdshot
-@end
--- ios/Pods/Target Support Files/Pods-CrowdshotTests/Pods-crowdshotTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CrowdshotTests : NSObject
-@end
-@implementation PodsDummy_Pods_CrowdshotTests
-@end
