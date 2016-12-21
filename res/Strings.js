d73d98e863d31675ed5cc619849b45c1847d709d
Kenneth Ma <ken@kenma.ca>
2016-12-20 23:43:32 -0500

    Changed: Firebase config moved to strings

--- res/Strings.js
+++ res/Strings.js
@@ -1,6 +1,13 @@
 export const Strings = {
   ClientVersion: 0.01,
-  StripeAPI: 'pk_test_TX7iEVi34oyuEqXVnOHByHOo'
+  StripeAPI: 'pk_test_TX7iEVi34oyuEqXVnOHByHOo',
+  FirebaseConfig: {
+    apiKey: "AIzaSyA2lybpyZB4tS50hvObKDrpS3jgnBIjV7E",
+    authDomain: "crowdshot-production.firebaseapp.com",
+    databaseURL: "https://crowdshot-production.firebaseio.com",
+    storageBucket: "crowdshot-production.appspot.com",
+    messagingSenderId: "129647293054"
+  }
 };
 
 export default Strings;
--- src/utils/Database.js
+++ src/utils/Database.js
@@ -1,13 +1,10 @@
 import * as firebase from 'firebase';
+import {
+  Strings
+} from '../Const';
 
-const config = {
-  apiKey: "AIzaSyA2lybpyZB4tS50hvObKDrpS3jgnBIjV7E",
-  authDomain: "crowdshot-production.firebaseapp.com",
-  databaseURL: "https://crowdshot-production.firebaseio.com",
-  storageBucket: "crowdshot-production.appspot.com",
-  messagingSenderId: "129647293054"
-};
-
-export const Firebase = firebase.initializeApp(config);
+export const Firebase = firebase.initializeApp(
+  Strings.FirebaseConfig
+);
 export const Database = Firebase.database();
 export default Database;
