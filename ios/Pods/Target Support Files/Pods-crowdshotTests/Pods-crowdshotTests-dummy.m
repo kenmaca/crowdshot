08a8cf4ceb8bd1f133ecbe273ff5e3fd8c49d055
Kenneth Ma <ken@kenma.ca>
2016-12-21 18:03:12 -0500

    Fixed: WIP Build Borks

--- ios/Pods/Target Support Files/Pods-crowdshotTests/Pods-crowdshotTests-dummy.m
+++ ios/Pods/Target Support Files/Pods-crowdshotTests/Pods-crowdshotTests-dummy.m
@@ -1,5 +1,5 @@
 #import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_crowdshotTests : NSObject
+@interface PodsDummy_Pods_CrowdshotTests : NSObject
 @end
-@implementation PodsDummy_Pods_crowdshotTests
+@implementation PodsDummy_Pods_CrowdshotTests
 @end
--- ios/Pods/Target Support Files/Pods-crowdshotTests/Pods-crowdshotTests-umbrella.h
+++ ios/Pods/Target Support Files/Pods-crowdshotTests/Pods-crowdshotTests-umbrella.h
@@ -3,6 +3,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_crowdshotTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_crowdshotTestsVersionString[];
+FOUNDATION_EXPORT double Pods_CrowdshotTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CrowdshotTestsVersionString[];
 
